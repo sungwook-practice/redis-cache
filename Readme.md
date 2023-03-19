636cc350754b8f95b26d09bfebd44c498e8fff2b
choisungwook <kgg1959@naver.com>
2023-03-19 22:59:16 +0900

    #2 get post구현 with redis

--- Readme.md
+++ Readme.md
@@ -1,8 +1,14 @@
 # 개요
 redis 캐시 연습
 
-# 실행방법
+# 준비
+* redis
+```shell
+# docker로 redis실행
+docker run -d -p 6379:6379 --name redis-test redis:7.0.9
+```
 
+# 실행방법
 ```shell
 cd app
 pip install requirements.txt
--- app/posts.py
+++ app/posts.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from database import get_redis_session
 import httpx
+import redis
+import json
 
 
 router = APIRouter(
@@ -9,14 +12,37 @@ router = APIRouter(
 
 @router.get('')
 def get_posts():
-    """get posts"""
-    response = httpx.get("https://jsonplaceholder.typicode.com/posts")
-    return response.json()
+  """get posts"""
+  response = httpx.get("https://jsonplaceholder.typicode.com/posts")
+  return response.json()
 
 
-# get post from id
 @router.get('/{id}')
-def get_post(id: int):
-    """get post from id"""
-    response = httpx.get(f"https://jsonplaceholder.typicode.com/posts/{id}")
-    return response.json()
+async def get_post(id: int, redis_session: redis.Redis = Depends(get_redis_session)):
+  """get post by id"""
+  post = None
+
+  try:
+    post = redis_session.get(id)
+  except (redis.exceptions.ConnectionError,
+          redis.exceptions.TimeoutError,
+          redis.exceptions.ResponseError):
+  # redis 장애로 간주
+    pass
+
+  if post:
+    return post
+
+  # cache hit을 하지 못한 경우 직접 API 호출
+  cache_ttl = 20
+  cache_key = f"post:{id}"
+
+  # try except
+  response = httpx.get(f"https://jsonplaceholder.typicode.com/posts/{id}")
+  post = response.json()
+
+  # write cache with TTL
+  # raise Invalid input of type: 'dict'. Convert to a bytes, string, int or float first..
+  redis_session.set(cache_key, json.dumps(post), ex=cache_ttl)
+
+  return post
